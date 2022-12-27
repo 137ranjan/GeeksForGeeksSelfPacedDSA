83797de418380917e734630f913b1e5cb8d6e4e9
Ranjan Singh <137ranjan@gmail.com>
2022-12-27 22:59:56 +0530

    Remove unused import

--- Recursion/RopeCuttingProblem/GFG.java
+++ Recursion/RopeCuttingProblem/GFG.java
@@ -1,28 +1,26 @@
 /*package whatever //do not write package name here */
 
-import java.io.*;
-
 class GFG {
-	public static void main (String[] args) {
-		System.out.println(maxPieces(9, 2, 2 , 2));
+	public static void main(String[] args) {
+		System.out.println(maxPieces(9, 2, 2, 2));
 	}
-	
-	private static int maxPieces(int n, int a, int b, int c){
-	    if(n == 0){
-	        return 0;
-	    }
-	    if(n < 0){
-	        return -1;
-	    }
-	    int x = maxPieces(n-a, a, b, c);
-        int y = maxPieces(n-b, a, b, c);
-        int z = maxPieces(n-c, a, b, c);
-        int res =  Math.max(Math.max(x,y),z);
-        
-        if(res == -1){
-            return -1;
-        }
-        
-        return res + 1;
+
+	private static int maxPieces(int n, int a, int b, int c) {
+		if (n == 0) {
+			return 0;
+		}
+		if (n < 0) {
+			return -1;
+		}
+		int x = maxPieces(n - a, a, b, c);
+		int y = maxPieces(n - b, a, b, c);
+		int z = maxPieces(n - c, a, b, c);
+		int res = Math.max(Math.max(x, y), z);
+
+		if (res == -1) {
+			return -1;
+		}
+
+		return res + 1;
 	}
 }

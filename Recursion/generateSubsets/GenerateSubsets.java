2b279fddcc87ab87f5c04bb90899fba68fe1351a
Ranjan Singh <137ranjan@gmail.com>
2022-12-28 21:22:32 +0530

    Add GenerateSubsets.java

---
+++ Recursion/generateSubsets/GenerateSubsets.java
@@ -0,0 +1,18 @@
+/**
+ * GenerateSubsets
+ */
+public class GenerateSubsets {
+
+    public static void main(String[] args) {
+        generateSubsets("ABC","",0);
+    }
+
+    public static void generateSubsets(String s, String curr, int i){
+        if (i == s.length()) {
+            System.out.println(curr);
+            return;
+        }
+        generateSubsets(s,curr,i+1);
+        generateSubsets(s,curr+s.charAt(i),i+1);
+    }
+}

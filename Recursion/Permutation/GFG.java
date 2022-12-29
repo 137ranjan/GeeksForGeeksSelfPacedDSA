ad5681c3c9a2ec29aa90391d2ac78dfe9b3a88fb
Ranjan Singh <137ranjan@gmail.com>
2022-12-29 21:18:18 +0530

    Add GFG.java for Permutation

---
+++ Recursion/Permutation/GFG.java
@@ -0,0 +1,34 @@
+/**
+ * GFG
+ */
+public class GFG {
+
+    public static void main(String[] args) {
+        String str = "ABC";
+        String[] s = str.split("");
+        permute(s,0);
+    }
+
+    private static void permute(String[] s, int i) {
+        if(i == s.length -1){
+            for (int k = 0; k < s.length; k++) {
+                System.out.print(s[k]);
+            }
+            System.out.print(" ");
+        }
+
+        for (int j = i; j < s.length; j++) {
+            String temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
+            permute(s, i+1);
+
+            String temp2 = s[i];
+            s[i] = s[j];
+            s[j] = temp2;
+
+        }
+    }
+
+}

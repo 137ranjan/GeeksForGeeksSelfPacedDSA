ce54afafe66dfea16bb6959db9132679ccc010d4
Ranjan Singh <137ranjan@gmail.com>
2022-12-29 21:20:14 +0530

    Formatting

--- Recursion/Permutation/GFG.cpp
+++ Recursion/Permutation/GFG.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void permute(string s, int i=0){
-    if(i == s.length() - 1){
-        cout<<s<<" ";
+void permute(string s, int i = 0)
+{
+    if (i == s.length() - 1)
+    {
+        cout << s << " ";
         return;
     }
-    for (int j = i; j < s.length() ; j++) {
-        swap(s[i],s[j]);
-        permute(s, i+1);
-        swap(s[i],s[j]);
+    for (int j = i; j < s.length(); j++)
+    {
+        swap(s[i], s[j]);
+        permute(s, i + 1);
+        swap(s[i], s[j]);
     }
 }
 
-int main() {
+int main()
+{
     permute("ABC");
-	return 0;
+    return 0;
 }
--- Recursion/Permutation/GFG.java
+++ Recursion/Permutation/GFG.java
@@ -6,11 +6,11 @@ public class GFG {
     public static void main(String[] args) {
         String str = "ABC";
         String[] s = str.split("");
-        permute(s,0);
+        permute(s, 0);
     }
 
     private static void permute(String[] s, int i) {
-        if(i == s.length -1){
+        if (i == s.length - 1) {
             for (int k = 0; k < s.length; k++) {
                 System.out.print(s[k]);
             }
@@ -22,7 +22,7 @@ public class GFG {
             s[i] = s[j];
             s[j] = temp;
 
-            permute(s, i+1);
+            permute(s, i + 1);
 
             String temp2 = s[i];
             s[i] = s[j];

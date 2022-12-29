7dd15dc11f8d137477128c9798ec66909ebf71aa
Ranjan Kumar Singh <39656138+137ranjan@users.noreply.github.com>
2022-12-29 19:25:12 +0530

    Create GFG.cpp for Permutation

---
+++ Recursion/Permutation/GFG.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void permute(string s, int i=0){
+    if(i == s.length() - 1){
+        cout<<s<<" ";
+        return;
+    }
+    for (int j = i; j < s.length() ; j++) {
+        swap(s[i],s[j]);
+        permute(s, i+1);
+        swap(s[i],s[j]);
+    }
+}
+
+int main() {
+    permute("ABC");
+	return 0;
+}

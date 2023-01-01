e6c2ebbd1aba37864c0a9ac5906b964b5896b105
Ranjan Singh <137ranjan@gmail.com>
2023-01-01 11:19:06 +0530

    Create DynamicArray.java for ArrayList in Java

---
+++ Arrays/DyanamicArray.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+/**
+ * DyanamicArray
+ */
+public class DyanamicArray {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrList = new ArrayList<>();
+        arrList.add(1);
+        arrList.add(2);
+        arrList.add(3);
+        for (Integer i : arrList) {
+            System.out.print(i + ", ");
+        }
+        System.out.println("\n");
+
+    }
+}

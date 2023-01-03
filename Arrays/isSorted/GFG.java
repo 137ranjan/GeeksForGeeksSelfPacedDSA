a2af7449c68e9c8de9425fd3a24fd142b3e81aa3
Ranjan Kumar Singh <39656138+137ranjan@users.noreply.github.com>
2023-01-03 11:52:11 +0530

    Create GFG.java for isSorted

---
+++ Arrays/isSorted/GFG.java
@@ -0,0 +1,19 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		int [] arr = {1,2,3,3,4,4,5};
+		System.out.println(isSorted(arr));
+	}
+	
+	private static boolean isSorted(int[] arr){
+	    for (int i=0; i<arr.length - 1; i++){
+	        if(arr[i] > arr[i+1]){
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+}

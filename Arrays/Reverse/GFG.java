0800bec909f7f8d523f4048ffbbc39f8999c0857
Ranjan Kumar Singh <39656138+137ranjan@users.noreply.github.com>
2023-01-03 13:35:44 +0530

    Create GFG.java for Reversing an Array

---
+++ Arrays/Reverse/GFG.java
@@ -0,0 +1,35 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		int [] arr = {1,2,3,3,4,4,5};
+		for(int i=0; i<arr.length; i++){
+		    System.out.print(arr[i]+" ");
+		}
+		System.out.println("\n");
+		reverse(arr);
+		
+		for(int i=0; i<arr.length; i++){
+		    System.out.print(arr[i]+" ");
+		}
+		
+		
+	}
+	
+	private static void reverse(int[] arr){
+	    
+	    int low = 0, high = arr.length - 1;
+	    
+	    while(low < high){
+	        
+	        int temp = arr[low];
+	        arr[low] = arr[high];
+	        arr[high] = temp;
+	        
+	        low++;
+	        high--;
+	    }
+	}
+}

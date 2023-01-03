3f686e6992ce716ad48724c61f46866ec07cd929
Ranjan Kumar Singh <39656138+137ranjan@users.noreply.github.com>
2023-01-03 11:16:50 +0530

    Create GFG.java for Second Largest Element Index

---
+++ Arrays/SeondLargestIndex/GFG.java
@@ -0,0 +1,36 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		int [] arr = new int[10];
+		arr[0] = 1;
+		arr[1] = 3;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 9;
+		arr[5] = 18;
+		int x = 7;
+		System.out.println(secondLargest(arr, 6));
+	}
+	
+	private static int secondLargest(int[] arr, int n){
+	   int max = 0;
+	   int secondMax = -1;
+	   
+	   for (int i=1; i<n; i++ ){
+	       if(arr[i] > arr[max]){
+	           secondMax = max;
+	           max = i;
+	           
+	       }else if(arr[i] != arr[max]){
+	          if(secondMax == -1 || arr[i] > arr[secondMax]){
+	              secondMax = i;
+	          }
+	       }
+	   }
+	   
+	   return secondMax;
+	}
+}

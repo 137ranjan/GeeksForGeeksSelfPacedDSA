28c554cf280a9dfeba1b8ecfb94065690f99a397
Ranjan Kumar Singh <39656138+137ranjan@users.noreply.github.com>
2023-01-03 16:13:38 +0530

    Create GFG.java
    for Removing Duplicates from Sorted Array

---
+++ Arrays/FindDuplicateSorted/GFG.java
@@ -0,0 +1,32 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		int [] arr = {1,2,3,3,4,4,5};
+		for(int i=0; i<arr.length; i++){
+		    System.out.print(arr[i]+" ");
+		}
+		System.out.println("\n");
+		int newSize = removeDuplicateSorted(arr,7);
+		
+		for(int i=0; i<newSize; i++){
+		    System.out.print(arr[i]+" ");
+		}
+		
+		
+	}
+	
+	private static int removeDuplicateSorted(int[] arr, int n){
+	   int res = 1; 
+	   for (int i=1;i<n ;i++ ){
+	       if(arr[i] != arr[res-1]){
+	           arr[res] = arr[i];
+	           res++;
+	       }
+	   }
+	   return res;
+	   
+	}
+}

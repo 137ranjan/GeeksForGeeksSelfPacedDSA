76423427c179bda1b7a7198013f18ae1af4f931e
Ranjan Singh <137ranjan@gmail.com>
2023-01-05 10:56:59 +0530

    format code

--- Arrays/StockBuyAndSellMaxProfitEfficient/GFG.java
+++ Arrays/StockBuyAndSellMaxProfitEfficient/GFG.java
@@ -20,10 +20,10 @@ public class GFG {
     }
 
     private static int maxProfit(int[] price) {
-        int profit = 0; 
+        int profit = 0;
         for (int i = 1; i < price.length; i++) {
-            if (price[i] > price[i-1]) {
-                profit += (price[i] - price[i-1]);
+            if (price[i] > price[i - 1]) {
+                profit += (price[i] - price[i - 1]);
             }
         }
         return profit;

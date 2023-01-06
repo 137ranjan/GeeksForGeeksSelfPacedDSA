7036efebf71c937944319d872d714ff7b7283814
Ranjan Kumar Singh <39656138+137ranjan@users.noreply.github.com>
2023-01-06 16:16:44 +0530

    Update Solution for Longest Even Odd Subarray

--- Arrays/LongestEvenOddSubarray/Main.java
+++ Arrays/LongestEvenOddSubarray/Main.java
@@ -1,3 +1,4 @@
+// "static void main" must be defined in a public class.
 import java.util.Scanner;
 
 public class Main {
@@ -5,36 +6,28 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<n; i++){
             arr[i] = input.nextInt();
         }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println("\n");
-        System.out.println(longestEvenOddSubArr(arr));
-        input.close();
+		for(int i=0; i<arr.length; i++){
+		    System.out.print(arr[i]+" ");
+		}
+		System.out.println("\n");
+		System.out.println(maxLengthEvenOddSubArr(arr));
     }
-
-    private static int longestEvenOddSubArr(int[] arr) {
+    
+    private static int maxLengthEvenOddSubArr(int[] arr){
         int n = arr.length;
-        int count = 0;
-        int maxCount1 = 0;
-        int maxCount2 = 0;
-        for (int i = 0; i < n-1; i += 2) {
-            if ((arr[i] % 2 == 0) && (arr[i + 1] % 2 != 0)) {
-                count++;
+        int maxCount = 1;
+        int currCount = 1;
+        for(int i=1; i<n; i++){
+            if( ((arr[i]%2 == 0) && (arr[i-1]%2 !=0)) || ((arr[i]%2 != 0) && (arr[i-1]%2 == 0)) ){
+                currCount++;
+                maxCount = Math.max(maxCount,currCount);
+            }else{
+                currCount = 1;
             }
-            maxCount1 = Math.max(maxCount1, count);
-            count = 0;
         }
-        for (int i = 0; i < n-1; i += 2) {
-            if ((arr[i] % 2 != 0) && (arr[i + 1] % 2 == 0)) {
-                maxCount2++;
-            }
-            maxCount2 = Math.max(maxCount2, count);
-            count = 0;
-        }
-        return (maxCount1 > maxCount2) ? maxCount1 : maxCount2;
+        return maxCount;
     }
 }

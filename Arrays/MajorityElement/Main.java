8e90ff5d5ac9b1cbc563f3ec9c912c18e1a17682
Ranjan Singh <137ranjan@gmail.com>
2023-01-07 14:10:56 +0530

    Solution to find Majority Element

---
+++ Arrays/MajorityElement/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("\n");
+        System.out.println(findMajority(arr));
+        input.close();
+    }
+
+    private static int findMajority(int[] arr) {
+        int res = 0, count = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[res] == arr[i]) {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                res = i;
+                count = 1;
+            }
+
+        }
+        count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[res] == arr[i]) {
+                count++;
+            }
+        }
+        if (count <= arr.length / 2) {
+            res = -1;
+        }
+        return res;
+    }
+}

70d0300c928da3bee3fb297ca9ba3a33a80a472a
Ranjan Kumar Singh <39656138+137ranjan@users.noreply.github.com>
2023-01-12 11:17:13 +0530

    Solution for Wave Array using sorted array

---
+++ Arrays/WaveArray/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static void convertToWave(int n, int[] a) {
+        // code here
+        for(int i=0; i< n - 1; i+=2){
+            int temp = a[i];
+            a[i] = a[i + 1];
+            a[i+1] = temp;
+        }
+    }
+}

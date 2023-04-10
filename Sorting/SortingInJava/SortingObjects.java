97c70e4a90e7830ca7cd7d5be3c2f9e4d7bad4f2
Ranjan Kumar Singh <137ranjan@gmail.com>
2023-04-10 11:39:34 +0530

    Formatting

--- Sorting/SortingInJava/SortingObjects.java
+++ Sorting/SortingInJava/SortingObjects.java
@@ -2,12 +2,10 @@ import java.util.Arrays;
 
 public class SortingObjects {
     public static void main(String[] args) {
-        Point[] arr = {new Point(10,20), new Point(3,12), new Point(5,7)};
+        Point[] arr = { new Point(10, 20), new Point(3, 12), new Point(5, 7) };
         Arrays.sort(arr);
         for (Point point : arr) {
-            System.out.println("("+point.x+", "+point.y+")");
+            System.out.println("(" + point.x + ", " + point.y + ")");
         }
     }
 }
-
-

05940f662c7d6fc8a3b7fd233a0c3810eab0836f
Ranjan Kumar Singh <39656138+137ranjan@users.noreply.github.com>
2023-04-23 12:00:52 +0530

    Triplet Sum in Array

---
+++ Sorting/TripletSumInArray/Solution.java
@@ -0,0 +1,31 @@
+class Solution
+{
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    public static boolean find3Numbers(int A[], int n, int X) { 
+    
+       // Your code Here
+       Arrays.sort(A);
+       
+       
+       for(int i=0; i<n-2; i++){
+           int l = i+1;
+           int r = n-1;
+           
+           while(l<r){
+               if(A[i]+A[l]+A[r] == X){
+                   return true;
+               }
+               else if(A[i]+A[l]+A[r] < X){
+                   l++;
+               }else {
+                   r--;
+               }
+           }
+           
+       }
+       
+       return false;
+    
+    }
+}

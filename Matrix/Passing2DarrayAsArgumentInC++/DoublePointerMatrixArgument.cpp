e0f83ac17cc94190cfdd942e6dbf252eb21a6dc5
Ranjan Singh <137ranjan@gmail.com>
2023-04-24 10:06:42 +0530

    Double Pointer used for Matrix argument

---
+++ Matrix/Passing2DarrayAsArgumentInC++/DoublePointerMatrixArgument.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+void print(int **arr, int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int **arr;
+    int m = 3, n = 2;
+    arr = new int *[m];
+    for (int i = 0; i < m; i++)
+    {
+        arr[i] = new int[n];
+        for (int j = 0; j < n; j++)
+        {
+            arr[i][j] = i;
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    print(arr, m, n);
+
+    return 0;
+}

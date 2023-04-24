d3fed48794b6d8d612213c7e821d6bba2c64ae1e
Ranjan Singh <137ranjan@gmail.com>
2023-04-24 10:29:45 +0530

    Vectors for Matrix Argument

---
+++ Matrix/Passing2DarrayAsArgumentInC++/VectorsMatrixArgument.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print(vector<int> arr[], int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < arr[i].size(); j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int m = 3, n = 2;
+    vector<int> arr[m];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i].push_back(i);
+        }
+    }
+    print(arr, m);
+    return 0;
+}

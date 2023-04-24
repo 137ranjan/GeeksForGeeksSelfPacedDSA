9b62d753d3d4f7bd2abd4af13546e0972e61e7c4
Ranjan Singh <137ranjan@gmail.com>
2023-04-24 15:09:41 +0530

    Matrix in Snake Pattern

---
+++ Matrix/MatrixInSnakePattern/Solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 }
+        };
+        // first implementation
+        for (int i = 0; i < arr.length; i += 2) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println(" ");
+            for (int k = arr[i + 1].length - 1; k >= 0; k--) {
+                System.out.print(arr[i + 1][k] + " ");
+            }
+            System.out.println(" ");
+        }
+        // second implementation
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
+            } else {
+                for (int j = arr[i].length - 1; j >= 0; j--) {
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+}

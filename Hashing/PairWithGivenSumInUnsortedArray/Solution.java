a44d2f408ec0a5627d7acd38b75316beebbf8b39
Ranjan Singh <137ranjan@gmail.com>
2023-04-30 17:23:10 +0530

    Removed Naive Approach

--- Hashing/PairWithGivenSumInUnsortedArray/Solution.java
+++ Hashing/PairWithGivenSumInUnsortedArray/Solution.java
@@ -6,11 +6,9 @@ public class Solution {
         int[] arr = { 3, 2, 8, 15, -8 };
         int sum = 17;
 
-        //System.out.println(pairWithGivenSum(arr, sum));
         System.out.println(isPair(arr, sum));
     }
 
-    // effecient
     private static boolean isPair(int[] arr, int sum) {
         Set<Integer> h = new HashSet<>();
         for (int x : arr) {
@@ -18,18 +16,6 @@ public class Solution {
                 return true;
             } else {
                 h.add(x);
-            }
-        }
-        return false;
-    }
-
-    // naive
-    private static boolean pairWithGivenSum(int[] arr, int sum) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] + arr[j] == sum) {
-                    return true;
-                }
             }
         }
         return false;

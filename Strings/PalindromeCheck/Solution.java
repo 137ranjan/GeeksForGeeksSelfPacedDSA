790f8e527bdfdffc5240230a7c402e4bbf0fd0d1
Ranjan Singh <137ranjan@gmail.com>
2023-05-07 17:21:51 +0530

    Palindrome Check

---
+++ Strings/PalindromeCheck/Solution.java
@@ -0,0 +1,23 @@
+import java.time.LocalDateTime;
+
+public class Solution {
+    public static void main(String[] args) {
+        String str = "abba";
+        isPalindrome(str);
+    }
+
+    private static void isPalindrome(String str) {
+        boolean flag = true;
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}

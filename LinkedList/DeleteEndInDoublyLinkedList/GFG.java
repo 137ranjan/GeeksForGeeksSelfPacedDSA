047e68cf1e3c520ad30b756f47a035905fd9107f
Ranjan Kumar Singh <39656138+137ranjan@users.noreply.github.com>
2023-10-09 14:39:22 +0530

    Create GFG.java

---
+++ LinkedList/DeleteEndInDoublyLinkedList/GFG.java
@@ -0,0 +1,56 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class Node{
+    int data;
+    Node prev;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        prev = null;
+        next = null;
+    }
+    
+}
+class GFG {
+	public static void main (String[] args) {
+		Node head = new Node(10);
+        Node temp1 = new Node(20);
+        Node temp2 = new Node(30);
+
+        head.next = temp1;
+        temp1.prev = head;
+        temp1.next = temp2;
+        temp2.prev = temp1;
+        
+		Node emptyNode = null;
+		head = deleteTail(head);
+        printLinkedList(head);
+	}
+	
+	public static Node deleteTail(Node head){
+	    if(head == null){
+	        return null;
+	    }
+	    if(head.next == null){
+	        return null;
+	    }else{
+	        Node curr = head;
+	        while(curr.next != null){
+	            curr = curr.next;
+	        }
+	        curr.prev.next = null;
+	        return head;
+	    }
+	}
+	
+	
+	public static void printLinkedList(Node head){
+	    while(head != null){
+	        System.out.print(head.data+" ");
+	        head = head.next;
+	    }
+	}
+}

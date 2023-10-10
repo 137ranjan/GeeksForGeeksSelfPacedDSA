c0b5cce320870809fce71959059bea35d675b41c
Ranjan Kumar Singh <39656138+137ranjan@users.noreply.github.com>
2023-10-10 08:39:15 +0530

    Insert at beginning of a linked list

---
+++ LinkedList/InsertBeginCircularLinkedList/GFG.java
@@ -0,0 +1,55 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG {
+	public static void main (String[] args) {
+		Node head = new Node(10);
+        Node temp1 = new Node(20);
+        Node temp2 = new Node(30);
+
+        head.next = temp1;
+        temp1.next = temp2;
+        temp2.next = head;
+        
+		Node emptyNode = null;
+		head = insertHeadCircularLinkedList(head, 2);
+        printCircularLinkedList(head);
+	}
+	public static Node insertHeadCircularLinkedList(Node head, int x){
+	    Node temp = new Node(x);
+	    if(head == null){
+	        temp.next = temp;
+	        return head;
+	    }else{
+	        Node curr = head;
+	        while(curr.next != head){
+	            curr = curr.next;
+	        }
+	        curr.next = temp;
+	        temp.next = head;
+	    }
+	    return temp;
+	}
+	
+	public static void printCircularLinkedList(Node head){
+	    if(head == null){
+	        return;
+	    }
+	    Node curr = head;
+	    do {
+	        System.out.print(curr.data+" ");
+	        curr = curr.next;
+	    } while(curr != head);
+	}
+}

54c84dc98a60ec4aee514e88b4c9318f60bcbcf4
Ranjan Kumar Singh <39656138+137ranjan@users.noreply.github.com>
2023-10-10 08:51:06 +0530

    Efficient insert at head in Circular Linked List

---
+++ LinkedList/InsertBeginCircularLinkedList/GFG2.java
@@ -0,0 +1,56 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG {
+	public static void main (String[] args) {
+		Node head = new Node(10);
+        Node temp1 = new Node(20);
+        Node temp2 = new Node(30);
+
+        head.next = temp1;
+        temp1.next = temp2;
+        temp2.next = head;
+        
+		Node emptyNode = null;
+		head = insertEfficientHeadCircularLinkedList(head, 2);
+        printCircularLinkedList(head);
+	}
+	public static Node insertEfficientHeadCircularLinkedList(Node head, int x){
+	    Node temp = new Node(x);
+	    if(head == null){
+	        temp.next = temp;
+	        return temp;
+	    }else{
+	        temp.next = head.next;
+	        head.next = temp;
+	        
+	        int t = head.data;
+	        head.data = temp.data;
+	        temp.data = t;
+	        
+	        return head;
+	    }
+	}
+	
+	public static void printCircularLinkedList(Node head){
+	    if(head == null){
+	        return;
+	    }
+	    Node curr = head;
+	    do {
+	        System.out.print(curr.data+" ");
+	        curr = curr.next;
+	    } while(curr != head);
+	}
+}

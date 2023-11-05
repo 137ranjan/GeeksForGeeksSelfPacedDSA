276c6663aff214946dfde64efa1e51d2256bfe75
Ranjan Kumar Singh <137ranjan@gmail.com>
2023-11-05 22:53:22 +0530

    Alternative methods which throw exceptions

--- Deque/DequeInJava/Solution.java
+++ Deque/DequeInJava/Solution.java
@@ -4,15 +4,15 @@ import java.util.LinkedList;
 public class Solution {
     public static void main(String[] args) {
         Deque<Integer> dq = new LinkedList<>();
-        dq.offerFirst(10);
-        dq.offerLast(20);
-        dq.offerFirst(5);
-        dq.offerLast(15);
-        System.out.println(dq.peekFirst());
-        System.out.println(dq.peekLast());
-        dq.pollFirst();
-        dq.pollLast();
-        System.out.println(dq.peekFirst());
-        System.out.println(dq.peekLast());
+        dq.offerFirst(10); // dq.addFirst(10);
+        dq.offerLast(20); // dq.addLast(20);
+        dq.offerFirst(5); // dq.addFirst(10);
+        dq.offerLast(15); // dq.addLast(20);
+        System.out.println(dq.peekFirst()); // dq.getFirst();
+        System.out.println(dq.peekLast()); // dq.getLast();
+        dq.pollFirst(); // dq.removeFirst();
+        dq.pollLast(); // dq.removeLast();
+        System.out.println(dq.peekFirst()); // dq.getFirst();
+        System.out.println(dq.peekLast()); // dq.getLast();
     }
 }

22d6cbff49315cb2c961dcd395ae39c7f64fff58
Ranjan Kumar Singh <137ranjan@gmail.com>
2023-11-05 23:03:57 +0530

    Iterator and Descending Iterator in Deque

--- Deque/DequeInJava/Solution.java
+++ Deque/DequeInJava/Solution.java
@@ -1,4 +1,5 @@
 import java.util.Deque;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Solution {
@@ -14,5 +15,20 @@ public class Solution {
         dq.pollLast(); // dq.removeLast();
         System.out.println(dq.peekFirst()); // dq.getFirst();
         System.out.println(dq.peekLast()); // dq.getLast();
+
+        Iterator<Integer> it = dq.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next()+" ");
+        }
+        System.out.println(" ");
+        for(int x: dq){
+            System.out.print(x+" ");
+        }
+        System.out.println(" ");
+        Iterator<Integer> dIt = dq.descendingIterator();
+        while (dIt.hasNext()) {
+            System.out.print(dIt.next()+" ");
+        }
+        System.out.println(" ");
     }
 }

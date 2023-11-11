49ddddafff1664b9b718fdfdce250a075eff35a9
Ranjan Kumar Singh <137ranjan@gmail.com>
2023-11-11 17:25:16 +0530

    Preorder tree traversal

---
+++ Trees/PreorderTraversal/Solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.right.left = new Node(40);
+        root.right.right = new Node(50);
+
+        preorder(root);
+    }
+
+    public static class Node {
+        int key;
+        Node left;
+        Node right;
+
+        Node(int key) {
+            this.key = key;
+        }
+    }
+
+    public static void preorder(Node root) {
+        if (root != null) {
+            System.out.print(root.key + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+}

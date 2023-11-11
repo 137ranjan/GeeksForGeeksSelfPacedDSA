735cc12f22df9eb6dcdba76a5905ee7665d49de8
Ranjan Kumar Singh <137ranjan@gmail.com>
2023-11-11 17:11:22 +0530

    Inorder Tree Traversal

---
+++ Trees/InorderTraversal/Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.right.left = new Node(40);
+        root.right.right = new Node(50);
+
+        inorder(root);
+    }
+
+    public static class Node {
+        int key;
+        Node left;
+        Node right;
+
+        Node(int key) {
+            this.key = key;
+        }
+    }
+
+    public static void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.key + " ");
+            inorder(root.right);
+        }
+
+    }
+}

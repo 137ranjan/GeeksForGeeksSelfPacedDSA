6a05db9c7e972ad905db4b896e0e39476e70b0c5
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-02-17 15:42:55 +0530

    Iterative Search in BST C++

---
+++ BinarySearchTree/SearchInBSTC++/IterativeSearchInBSTC++/Solution.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int key;
+    Node *left;
+    Node *right;
+
+    Node(int x)
+    {
+        key = x;
+        left = right = NULL;
+    }
+};
+bool search(Node *root, int x)
+{
+    while (root != NULL)
+    {
+        if (root->key == x)
+            return true;
+        if (root->key > x)
+            root = root->left;
+        else
+            root = root->right;
+    }
+    return false;
+}
+int main()
+{
+    Node *root = new Node(15);
+    root->left = new Node(5);
+    root->right = new Node(20);
+    root->left->left = new Node(3);
+    root->right->left = new Node(18);
+    root->right->left = new Node(16);
+    root->right->right = new Node(80);
+
+    cout << search(root, 16);
+}

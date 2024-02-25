162c84ee5c695439fe29bd47a61987241466e974
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-02-25 11:43:53 +0530

    Bottom View of Binary Tree

---
+++ BinarySearchTree/BottomViewOfBinaryTree/Solution.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
+
+public class Solution {
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+
+        bottomViewOfBinaryTree(root);
+    }
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static class Pair {
+        Node node;
+        int hd;
+
+        Pair(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+
+    static void bottomViewOfBinaryTree(Node root) {
+        Queue<Pair> q = new LinkedList<>();
+        Map<Integer, ArrayList<Integer>> map = new TreeMap<>();
+        q.add(new Pair(root, 0));
+        while (!q.isEmpty()) {
+            Pair p = q.poll();
+            Node curr = p.node;
+            int hd = p.hd;
+            if (map.containsKey(hd)) {
+                map.get(hd).add(0, curr.data);
+                map.get(hd).remove(1);
+            } else {
+                ArrayList<Integer> al = new ArrayList<>();
+                al.add(curr.data);
+                map.put(hd, al);
+            }
+            if (curr.left != null) {
+                q.add(new Pair(curr.left, hd - 1));
+            }
+            if (curr.right != null) {
+                q.add(new Pair(curr.right, hd + 1));
+            }
+        }
+        for (ArrayList<Integer> al : map.values()) {
+            for (int item : al) {
+                System.out.print(item + " ");
+            }
+            // System.out.println();
+        }
+    }
+}

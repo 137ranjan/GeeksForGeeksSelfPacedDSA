eac7071fb4d214f1ede83da920b6f315db883382
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-03-19 12:39:46 +0530

    BFS for directed graph

---
+++ Graph/BreadthFirstSearchDirectedGraph/Solution.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int v = 7;
+        for (int i = 0; i < v; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        addEdge(adj, 0, 1);
+        addEdge(adj, 0, 2);
+        addEdge(adj, 1, 3);
+        addEdge(adj, 2, 3);
+        addEdge(adj, 4, 5);
+        addEdge(adj, 4, 6);
+        addEdge(adj, 5, 6);
+
+        printGraph(adj);
+
+        bfsDirected(adj, v);
+
+    }
+
+    public static void printGraph(ArrayList<ArrayList<Integer>> adj) {
+        for (int i = 0; i < adj.size(); i++) {
+            System.out.print(i + " -> ");
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                System.out.print(adj.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v) {
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+
+    public static void bfs(ArrayList<ArrayList<Integer>> adj, int s, boolean[] visited) {
+        Queue<Integer> q = new LinkedList<>();
+        visited[s] = true;
+        q.add(s);
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            System.out.print(u + " ");
+            for (int v : adj.get(u)) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    q.add(v);
+                }
+            }
+        }
+    }
+
+    public static void bfsDirected(ArrayList<ArrayList<Integer>> adj, int v) {
+        boolean[] visited = new boolean[v + 1];
+        for (int i = 0; i < v; i++) {
+            if (!visited[i]) {
+                bfs(adj, i, visited);
+            }
+        }
+    }
+}

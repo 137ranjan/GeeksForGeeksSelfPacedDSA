59686a1531c5aaf9f5f3ff65807efcafeb929728
Ranjan Kumar Singh <39656138+137ranjan@users.noreply.github.com>
2024-03-20 16:07:20 +0530

    Depth First Search

---
+++ Graph/DFS/GFG.java
@@ -0,0 +1,53 @@
+/*package whatever //do not write package name here */
+
+import java.util.ArrayList;
+
+class GFG {
+    
+	public static void main (String[] args) {
+	   ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+	   int v = 5;
+	   for(int i=0; i<v; i++){
+	       adj.add(new ArrayList<>());
+	   }
+	   addEdge(adj, 0,1);
+	   addEdge(adj, 0,2);
+	   addEdge(adj, 1,2);
+	   addEdge(adj, 3,4);
+	   
+	   printGraph(adj);
+	   
+	   dfs(adj, v, 0);
+	}
+	
+	private static void printGraph(ArrayList<ArrayList<Integer>> adj){
+	    for(int i=0; i<adj.size(); i++){
+	        System.out.print(i+" -> ");
+	        for(int j=0; j<adj.get(i).size(); j++){
+	            System.out.print(adj.get(i).get(j)+" ");
+	        }
+	        System.out.println();
+	    }
+	}
+	
+	private static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v){
+	    adj.get(u).add(v);
+	    adj.get(v).add(u);
+	}
+	
+	private static void dfsRec(ArrayList<ArrayList<Integer>> adj, int s, boolean[] visited){
+	    visited[s] = true;
+	    System.out.print(s + " ");
+	    for(int u: adj.get(s)){
+	        if(visited[u] == false){
+	            dfsRec(adj, u, visited);
+	        }
+	    }
+	}
+	
+	private static void dfs(ArrayList<ArrayList<Integer>> adj, int v, int s ){
+	    boolean[] visited = new boolean[v]; 
+	    dfsRec(adj, s, visited);
+	}
+	
+}

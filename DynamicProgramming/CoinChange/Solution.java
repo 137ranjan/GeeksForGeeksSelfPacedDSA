bec5d335dd319a9028389ebfc5e9c7cedcd309a6
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-05-01 22:29:06 +0530

    Coin change problem : DP solution

--- DynamicProgramming/CoinChange/Solution.java
+++ DynamicProgramming/CoinChange/Solution.java
@@ -5,6 +5,7 @@ public class Solution {
         int s = 5;
 
         System.out.println(countWays(coins, n, s));
+        System.out.println(countWaysDp(coins, n, s));
     }
 
     // Naive Recursive solution
@@ -18,5 +19,27 @@ public class Solution {
         if (n == 0)
             return 0;
         return countWays(coins, n, s - coins[n - 1]) + countWays(coins, n - 1, s);
+    }
+
+    // Dp solution
+    private static int countWaysDp(int coins[], int n, int s) {
+        int[][] dp = new int[n + 1][s + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j <= s; j++) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= s; j++) {
+                if (coins[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                }
+            }
+        }
+
+        return dp[n][s];
     }
 }

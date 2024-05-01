44236c7313c61590377dfbbb2c1a3e4bfc1b38f7
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-05-01 23:37:58 +0530

    Edit Distance DP solution

--- DynamicProgramming/EditDistance/Solution.java
+++ DynamicProgramming/EditDistance/Solution.java
@@ -5,6 +5,7 @@ public class Solution {
         int m = s1.length();
         int n = s2.length();
         System.out.println(editDistance(s1, s2, m, n));
+        System.out.println(editDistanceDP(s1, s2, m, n));
     }
 
     // Edit Distance recursive solution
@@ -22,5 +23,25 @@ public class Solution {
                     Math.min(editDistance(s1, s2, m - 1, n),
                             editDistance(s1, s2, m - 1, n - 1)));
         }
+    }
+
+    private static int editDistanceDP(String s1, String s2, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1]));
+                }
+            }
+        }
+        return dp[m][n];
     }
 }

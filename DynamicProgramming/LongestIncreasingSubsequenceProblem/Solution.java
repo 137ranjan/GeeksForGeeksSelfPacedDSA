e955127f7c5c8dd04d5c658f5bf1e9c8f0de439d
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-05-03 09:05:02 +0530

    Longest Increasing subsequence problem: O(nlog n)

--- DynamicProgramming/LongestIncreasingSubsequenceProblem/Solution.java
+++ DynamicProgramming/LongestIncreasingSubsequenceProblem/Solution.java
@@ -3,6 +3,7 @@ public class Solution {
         int[] arr = { 10, 5, 18, 7, 2, 9 };
         int n = arr.length;
         System.out.println(longestIncSubseq(arr, n));
+        System.out.println(lisEfficient(arr, n));
     }
 
     // longest increasing subsequence: Time complexity = O(n^2)
@@ -23,5 +24,35 @@ public class Solution {
             res = Math.max(res, lis[i]);
         }
         return res;
+    }
+
+    private static int lisEfficient(int[] arr, int n) {
+        int[] tail = new int[n];
+        int len = 1;
+        tail[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > tail[len - 1]) {
+                tail[len] = arr[i];
+                len++;
+            } else {
+                int c = cielIndex(tail, 0, len - 1, arr[i]);
+                tail[c] = arr[i];
+            }
+        }
+
+        return len;
+    }
+
+    private static int cielIndex(int[] tail, int l, int r, int x) {
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (tail[m] >= x) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return r;
     }
 }

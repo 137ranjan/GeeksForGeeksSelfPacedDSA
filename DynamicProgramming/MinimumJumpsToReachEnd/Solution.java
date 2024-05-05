d46b01507ba3b1a2badcbd529cd0c4732f0ce662
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-05-05 23:53:29 +0530

    Minimum Jumps to Rach End DP Solution

--- DynamicProgramming/MinimumJumpsToReachEnd/Solution.java
+++ DynamicProgramming/MinimumJumpsToReachEnd/Solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public static void main(String[] args) {
         int[] arr = { 3, 4, 0, 1, 2, 1 };
         int[] arr2 = { 4, 1, 5, 3, 1, 3, 2, 1, 8 };
-        System.out.println(minJumps(arr, 5));
+        System.out.println(minJumpsDP(arr, 5));
         // System.out.println(minJumps(arr2, 0, 0));
     }
 
@@ -25,7 +25,6 @@ public class Solution {
         return res;
     }
 
-
     // gfg solution
     private static int minJumps(int[] arr, int n) {
         if (n == 1) {
@@ -36,10 +35,29 @@ public class Solution {
             if (i + arr[i] >= n - 1) {
                 int subRes = minJumps(arr, i + 1);
                 if (subRes != Integer.MAX_VALUE) {
-                    res = Math.min(res, subRes+1);
+                    res = Math.min(res, subRes + 1);
                 }
             }
         }
         return res;
+    }
+
+    private static int minJumpsDP(int[] arr, int n) {
+        int[] dp = new int[n];
+        dp[0] = 0;
+        for (int i = 1; i < n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] + j >= i) {
+                    if (dp[j] != Integer.MAX_VALUE) {
+                        dp[i] = Math.min(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+        }
+
+        return dp[n-1];
     }
 }

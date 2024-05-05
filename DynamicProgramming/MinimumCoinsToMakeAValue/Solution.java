8152dfedd5c1c0e10019301a9edf4112b430aafe
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-05-05 12:00:22 +0530

    Minimum Coins to make a value, DP solution

--- DynamicProgramming/MinimumCoinsToMakeAValue/Solution.java
+++ DynamicProgramming/MinimumCoinsToMakeAValue/Solution.java
@@ -2,6 +2,7 @@ public class Solution {
     public static void main(String[] args) {
         int[] coins = { 25, 10, 5 };
         System.out.println(getMin(coins, 3, 30));
+        System.out.println(getMinDP(coins, 3, 30));
     }
 
     private static int getMin(int[] coins, int n, int val) {
@@ -18,5 +19,25 @@ public class Solution {
         }
 
         return res;
+    }
+
+    private static int getMinDP(int[] coins, int n, int val) {
+        int[] dp = new int[val + 1];
+        dp[0] = 0;
+        for (int i = 1; i < val + 1; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+        for (int i = 1; i <= val; i++) {
+            for (int j = 0; j < n; j++) {
+                if (coins[j] <= i) {
+                    int subRes = dp[i - coins[j]];
+                    if (subRes != Integer.MAX_VALUE) {
+                        dp[i] = Math.min(dp[i], subRes + 1);
+                    }
+                }
+            }
+        }
+
+        return dp[val];
     }
 }

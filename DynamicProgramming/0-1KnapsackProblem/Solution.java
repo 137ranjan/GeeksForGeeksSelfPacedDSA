b6c2591abb232c45a2c2450ed16941ac3600a039
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-05-07 08:50:17 +0530

    0-1 Knapsack Problem DP solution

--- DynamicProgramming/0-1KnapsackProblem/Solution.java
+++ DynamicProgramming/0-1KnapsackProblem/Solution.java
@@ -1,21 +1,44 @@
-public class Solution{
-    public static void main(String[] args){
-        int[] val = {10, 40, 30, 50};
-        int[] wt = {5, 4, 6, 3};
+public class Solution {
+    public static void main(String[] args) {
+        int[] val = { 10, 40, 30, 50 };
+        int[] wt = { 5, 4, 6, 3 };
         int w = 10;
         int n = wt.length;
         System.out.println(knapsack(w, wt, val, n));
+        System.out.println(knapsackDP(w, wt, val, n));
     }
 
-    private static int knapsack(int w, int[] wt, int[] val, int n){
-        if(n == 0 || w == 0){
+    private static int knapsack(int w, int[] wt, int[] val, int n) {
+        if (n == 0 || w == 0) {
             return 0;
         }
-        if(wt[n-1] > w){
-            return knapsack(w, wt, val, n-1);
+        if (wt[n - 1] > w) {
+            return knapsack(w, wt, val, n - 1);
+        } else {
+            return Math.max(knapsack(w, wt, val, n - 1), val[n - 1] + knapsack(w - wt[n - 1], wt, val, n - 1));
         }
-        else{
-            return Math.max(knapsack(w, wt, val, n-1), val[n-1]+knapsack(w-wt[n-1], wt, val, n-1));
+    }
+
+    private static int knapsackDP(int w, int[] wt, int[] val, int n) {
+        int[][] dp = new int[n + 1][w + 1];
+        for (int i = 0; i <= w; i++) {
+            dp[0][i] = 0;
         }
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= w; j++) {
+                if (wt[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+                }
+            }
+        }
+
+        return dp[n][w];
+
     }
 }

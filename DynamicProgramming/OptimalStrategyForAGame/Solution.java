8fa7d44997f857fa23cc66e3475a09b4ba9af316
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-05-09 09:51:53 +0530

    Optimal Strategy For a game: DP Solution

--- DynamicProgramming/OptimalStrategyForAGame/Solution.java
+++ DynamicProgramming/OptimalStrategyForAGame/Solution.java
@@ -29,4 +29,22 @@ public class Solution {
         return Math.max(arr[i] + Math.min(sol2(arr, i + 2, j), sol2(arr, i + 1, j - 1)),
                 arr[j] + Math.min(sol2(arr, i + 1, j - 1), sol2(arr, i, j - 2)));
     }
+
+    // DP solution based on Recursive solution 2
+    public static int sol2DP(int[] arr) {
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n - 1; i++) {
+            dp[i][i + 1] = Math.max(arr[i], arr[i + 1]);
+        }
+        for (int gap = 3; gap < n; gap = gap + 2) {
+            for (int i = 0; i + gap < n; i++) {
+                int j = i + gap;
+                dp[i][j] = Math.max(arr[i] + Math.min(dp[i + 1][j], dp[i + 1][j - 1]),
+                        arr[j] + Math.min(dp[i + 1][j - 1], dp[i][j - 2]));
+            }
+        }
+
+        return dp[0][n - 1];
+    }
 }

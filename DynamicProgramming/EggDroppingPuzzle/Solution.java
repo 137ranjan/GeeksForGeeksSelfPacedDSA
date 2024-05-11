fbb175d1fcf58093f01dd0a6cd77f48b054ef3b7
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-05-11 08:27:34 +0530

    Egg Dropping Puzzle: DP Solution

--- DynamicProgramming/EggDroppingPuzzle/Solution.java
+++ DynamicProgramming/EggDroppingPuzzle/Solution.java
@@ -1,10 +1,12 @@
 public class Solution {
     public static void main(String[] args) {
         System.out.println(eggDrop(10, 2));
+        System.out.println(eggDropDP(10, 2));
     }
 
     /*
-     * Function to get minimum number of trials needed in worst case with n eggs and
+     * Recursive Function to get minimum number of trials needed in worst case with
+     * n eggs and
      * k floors
      */
     private static int eggDrop(int f, int e) {
@@ -24,7 +26,7 @@ public class Solution {
         int x;
         int res;
         for (x = 1; x <= f; x++) {
-            res = Math.max(eggDrop(f - 1, e - 1), eggDrop(f - x, e));
+            res = Math.max(eggDrop(x - 1, e - 1), eggDrop(f - x, e));
 
             if (res < min) {
                 min = res;
@@ -32,5 +34,26 @@ public class Solution {
         }
 
         return min + 1;
+    }
+
+    // Egg Dropping Puzzle DP Solution
+    private static int eggDropDP(int f, int e) {
+        int[][] dp = new int[f + 1][e + 1];
+        for (int i = 1; i <= e; i++) {
+            dp[1][i] = 1;
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i <= f; i++) {
+            dp[i][1] = i;
+        }
+        for (int i = 2; i <= f; i++) {
+            for (int j = 2; j <= e; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int x = 1; x <= i; x++) {
+                    dp[i][j] = Math.min(dp[i][j], 1 + Math.max(dp[x - 1][j - 1], dp[i - x][j]));
+                }
+            }
+        }
+        return dp[f][e];
     }
 }

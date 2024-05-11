e6f0782119aaf2487f808dc187227c7b596d0e52
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-05-11 19:06:30 +0530

    Maximum sum with no two consecutive: DP solution

--- DynamicProgramming/MaximumSumWithNoTwoConsecutive/Solution.java
+++ DynamicProgramming/MaximumSumWithNoTwoConsecutive/Solution.java
@@ -2,6 +2,7 @@ public class Solution {
     public static void main(String[] args) {
         int[] arr = { 10, 5, 15, 20 };
         System.out.println(maxSum(arr, arr.length));
+        System.out.println(maxSumDP(arr, arr.length));
     }
 
     // Recursive Solution
@@ -13,5 +14,19 @@ public class Solution {
         } else {
             return Math.max(maxSum(arr, n - 1), maxSum(arr, n - 2) + arr[n - 1]);
         }
+    }
+
+    // DP Solution
+    private static int maxSumDP(int[] arr, int n) {
+        if (n == 1) {
+            return arr[0];
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = arr[0];
+        dp[2] = Math.max(arr[0], arr[1]);
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + arr[i - 1]);
+        }
+        return dp[n];
     }
 }

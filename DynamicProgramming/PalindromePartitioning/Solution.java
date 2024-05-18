498287259f9bef9b3f96a4cf6aa416829c9940ca
Ranjan Kumar Singh <137ranjan@gmail.com>
2024-05-18 11:16:45 +0530

    Palindrome Partitioning: DP Solution

--- DynamicProgramming/PalindromePartitioning/Solution.java
+++ DynamicProgramming/PalindromePartitioning/Solution.java
@@ -4,6 +4,7 @@ class Solution {
         int i = 0;
         int j = 3;
         System.out.println(palPart(s, i, j));
+        System.out.println(palPartDP(s));
     }
 
     private static boolean isPalindrome(String s, int i, int j) {
@@ -27,5 +28,28 @@ class Solution {
             res = Math.min(res, 1 + palPart(s, i, k) + palPart(s, k + 1, j));
         }
         return res;
+    }
+
+    // DP solution
+    private static int palPartDP(String str) {
+        int n = str.length();
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 0;
+        }
+        for (int gap = 1; gap < n; gap++) {
+            for (int i = 0; i + gap < n; i++) {
+                int j = i + gap;
+                if (isPalindrome(str, i, j)) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = Integer.MAX_VALUE;
+                    for (int k = i; k < j; k++) {
+                        dp[i][j] = Math.min(dp[i][j], 1 + dp[i][k] + dp[k + 1][j]);
+                    }
+                }
+            }
+        }
+        return dp[0][n - 1];
     }
 }
